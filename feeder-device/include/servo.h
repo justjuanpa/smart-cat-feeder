e7e43707d1176baf4c66f5ea8815c78972e77d02
JayR-360 <149827065+JayR-360@users.noreply.github.com>
2026-07-15 19:47:43 -0400

    Add status and manual control functions for servos

--- feeder-device/include/servo.h
+++ feeder-device/include/servo.h
@@ -13,4 +13,10 @@ void servoRotate_task(void *parameters);
 void servoEnableLeft(bool val);
 void servoEnableRight(bool val);
 
+bool rightServoStatus(void);
+bool leftServoStatus(void);
+void manual_right_servo_task(void *para);
+void manual_left_servo_task(void *para);
+
+
 #endif // SERVO_H
